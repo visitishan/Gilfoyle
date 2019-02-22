6222e53a71d0b6e4e57942d9951b1365417673d4
Ishan Jain <visitishan@gmail.com>
2019-02-22 16:30:58 +0530

    Added stop loss limits
    - created config file for customizing
    - Added upper limit and lower limits
    - Modified for selecting other currencies too

---
+++ config.py
@@ -0,0 +1,11 @@
+# Avalible currencies to select for coin - ["ETH","BTC","LTC","XRP","OMG","REQ","ZRX","GNT","BAT","AE","TRX","XLM","NEO","GAS","XRB","NCASH","EOS","CMT","ONT","ZIL","IOST","ACT","ZCO","SNT","POLY","ELF","REP","QKC","XZC","BCHABC","TUSD","BCHSV", "BTT"]
+coin = 'BTC'
+
+#refresh interval for price in seconds
+refresh_interval = 10
+
+#Stop Loss Upper Limit in INR
+upper_limit = 285990
+
+#Stop Loss Lower Limit in INR
+lower_limit = 200000
--- pyticker.py
+++ pyticker.py
@@ -1,10 +1,10 @@
 import requests
 from playsound import playsound
 from time import sleep
-from os import system, name 
+from os import system, name
+from config import * 
 
 infinite = 1
-coin = 'BTC'
 r = requests.get('https://koinex.in/api/ticker')
 jsonfile = r.json()
 ok = jsonfile['prices']['inr'][coin]
@@ -21,10 +21,10 @@ def clear():
 
 
 while(infinite==1):
-	if ok >= 26000:
+	if (ok >= upper_limit or ok < lower_limit):
 		playsound('alert.mp3')
 	else:
 		print('<')
 	clear()
-	print(ok)
-	sleep(5)
+	print('Price of '+ coin + ' in INR is -   Rs. ' + str(ok))
+	sleep(refresh_interval)
